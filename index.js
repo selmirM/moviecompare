c33641ca92b2aab0e7ff1591307a250b6923b34c
smujezinovic <smujezinovic@capeannenterprises.com>
2020-05-14 20:47:10 +0200

    Add OMDBAPI call; Add utils file with debounce function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd9835cc5',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+onInput = event => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 1000));
